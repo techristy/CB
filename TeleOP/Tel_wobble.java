c45802f7719b8b4f05d7873f0a4592214b324caf
Barathgmk <barath.gmk@gmail.com>
2021-01-02 11:14:00 -0500

    New updates

--- TeleOP/Tel_wobble.java
+++ TeleOP/Tel_wobble.java
@@ -42,8 +42,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.vuforia.CameraDevice;
-
-
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
